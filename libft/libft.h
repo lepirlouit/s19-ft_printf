89c959c5901a1b7322fc58518a05b7378e90717b
Benoît de Biolley <bdb@aloalto.com>
2022-06-05 17:21:02 +0200

    :fire: remove unused code

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: bde-biol <bde-biol@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 09:08:05 by bde-biol          #+#    #+#             */
-/*   Updated: 2022/06/04 18:45:59 by bde-biol         ###   ########.fr       */
+/*   Updated: 2022/06/05 17:19:15 by bde-biol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,6 @@
 # include <stdlib.h>
 
 size_t	ft_strlen(const char *s);
-void	ft_putchar_fd(char c, int fd);
-void	ft_putstr_fd(char *s, int fd);
-void	ft_putnbr_fd(int n, int fd);
 int		ft_isdigit(int c);
 int		ft_atoi(const char *nptr);
 
